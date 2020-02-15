69ddffad2bc7162f75884e01840c1889d3a01086
Mukund Raj <54507726+Mukund-Raj@users.noreply.github.com>
2020-02-15 14:25:29 +0530

    making line between dots

---
+++ Assets/Scripts/DetectMouseAndDrawLine.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class DetectMouseAndDrawLine : MonoBehaviour,IPointerClickHandler,IDragHandler,IPointerEnterHandler
+{
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (eventData.dragging)
+        {
+            Debug.Log("user dragging");
+            Debug.Log(Input.mousePosition);
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.dragging)
+        {
+            Debug.Log("user dragging");
+            Debug.Log(Input.mousePosition);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Mouse entered");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/DrawLineBetweenTwoDots.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrawLineBetweenTwoDots : MonoBehaviour
+{
+    public GameObject obj1;
+    public GameObject obj2;
+    public LineRenderer line;
+    public Camera c;
+
+     void Start()
+    {
+        Camera cam = Camera.main;
+        Vector3 corners = new Vector3(Screen.width, Screen.height, 0f);
+        //Debug.Log(obj1);
+        Vector3 pos = obj1.GetComponent<RectTransform>().localPosition;
+        Debug.Log(cam.WorldToScreenPoint(pos));
+        Vector3 pos1 = obj2.GetComponent<RectTransform>().localPosition;
+        Debug.Log(cam.WorldToScreenPoint(pos1));
+        Debug.Log(cam.)
+        Debug.Log(cam.ScreenToWorldPoint(corners));
+        Debug.Log(cam.WorldToViewportPoint(c.transform.position));
+        
+        //Debug.Log("dot 1" + obj1.GetComponent<RectTransform>().localPosition);
+        //Debug.Log("dot 2"+obj2.GetComponent<RectTransform>().localPosition);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+    }
+}
